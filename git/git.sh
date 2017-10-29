6309ad37eaad43e11fee6e3530a7360569c67ec1
lx106 <1062461248@qq.com>
2017-10-29 14:58:41 +0800

    initial

---
+++ git/git.sh
@@ -0,0 +1,16 @@
+git init
+echo Change me > change-me
+echo Delete me > delete-me
+git add change-me delete-me
+git commit -m initial
+
+echo OK >> change-me
+rm delete-me
+echo Add me > add-me
+
+git status
+# Changed but not updated:
+#   modified:   change-me
+#   deleted:    delete-me
+# Untracked files:  表示新增的文件，还没有任何git操作
+#   add-me
