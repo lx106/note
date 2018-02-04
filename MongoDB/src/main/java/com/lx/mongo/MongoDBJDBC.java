0f372807264d5160e12397790148f02bd61541c7
liuxun123 <3073820738@qq.com>
2018-02-04 22:01:58 +0800

    添加mongodb

--- MongoDB/src/main/java/com/lx/mongo/MongoDBJDBC.java
+++ MongoDB/src/main/java/com/lx/mongo/MongoDBJDBC.java
@@ -1,6 +1,9 @@
 package com.lx.mongo;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
@@ -9,6 +12,7 @@ import com.mongodb.client.model.Filters;
 import org.bson.Document;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MongoDBJDBC {
@@ -16,24 +20,25 @@ public class MongoDBJDBC {
         try {
             // 连接到 mongodb 服务
             MongoClient mongoClient = new MongoClient("localhost", 27017);
-
             // 连接到数据库
-            MongoDatabase mongoDatabase = mongoClient.getDatabase("admin");
+            //MongoDatabase mongoDatabase = mongoClient.getDatabase("monitor");
+            MongoDatabase mongoDatabase = getDateBase("admin");
+            System.out.println(mongoDatabase);
             System.out.println("Connect to database successfully");
 
 //            mongoDatabase.createCollection("test");
 //            System.out.println("集合创建成功");
-            MongoCollection<Document> collection = mongoDatabase.getCollection("test");
-            /*System.out.println("集合 test 选择成功");
+            MongoCollection<Document> collection = mongoDatabase.getCollection("monitor");
+            System.out.println("集合 test 选择成功");
 
-            Document document = new Document("title", "MongoDB").
+            Document document = new Document("monitor", "MongoDB").
                                                                 append("description", "database").
                                                                 append("likes", 100).
                                                                 append("by", "Fly");
             List<Document> documents = new ArrayList<Document>();
             documents.add(document);
             collection.insertMany(documents);
-            System.out.println("文档插入成功");*/
+            System.out.println("文档插入成功");
 
             collection.updateMany(Filters.eq("likes", 100), new Document("$set",new Document("likes",200)));
 
@@ -48,5 +53,28 @@ public class MongoDBJDBC {
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
+    }
+
+    public static MongoDatabase getDateBase(String db){
+        MongoDatabase database = null;
+        try {
+            //连接数据库 start
+            MongoCredential credential = MongoCredential.createCredential("zhongzhu", db, "111111".toCharArray());
+//          MongoCredential credential = MongoCredential.createMongoCRCredential("yutao", db, "yutao".toCharArray());
+            ServerAddress serverAddress;
+            serverAddress = new ServerAddress("10.136.15.102", 27017);
+            List<ServerAddress> seeds = new ArrayList<ServerAddress>();
+            seeds.add(serverAddress);
+            List<MongoCredential> credentials = new ArrayList<MongoCredential>();
+            credentials.add(credential);
+//          @SuppressWarnings("resource")
+            MongoClient mongoClient = new MongoClient(seeds, credentials);
+            database = mongoClient.getDatabase(db);
+            System.out.println("Connect to database successfully");
+            //连接数据库 end
+        } catch (Exception e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+        return database;
     }
 }
